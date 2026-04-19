abe4f12c710d933491645173eebe7e4f1c6e592b
ezrentin <ezrentin1@gmail.com>
2026-04-19 15:30:43 +0530

    Update application versioning in `app/build.gradle.kts`.
    Key changes:
    - Incremented `versionCode` from `1` to `2`.
    - Updated `versionName` from `"1"` to `"1.1"`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mobile.fingram"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "com.mobile.fingram.CustomTestRunner"
     }
 
